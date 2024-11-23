9ecf995cf5ada959918bfa4daa92cc50144cda30
Brian West <bwest@xometry.com>
2024-11-23 10:50:41 -0500

    add strategy tables

--- python/blackjack.py
+++ python/blackjack.py
@@ -4,8 +4,12 @@ from enum import StrEnum
 import random
 import pdb
 
-# seed the random number generator to make game play reproducible:
+# seed the random number generator to make game play 100% reproducible:
 random.seed(0xDEADBEEF)
+
+#
+# Card and Deck and Shoe
+#
 
 
 class CardSuit(StrEnum):
@@ -19,9 +23,6 @@ class CardSuit(StrEnum):
 # print(f"{ord('♥️'[0]): #04x}") # 0x02665
 # print(f"{ord('♥️'[1]): #04x}") # 0x0fe0f
 # print("\u2665\ufe0f")
-
-# convert enum to string:
-# print(f"""Black spade: {CardSuit.SPADES.value}.""")
 
 
 class CardRank(StrEnum):
@@ -125,11 +126,6 @@ UNSHUFFLED_DECK: list[Card] = [
     Card(suite=CardSuit.CLUBS, rank=CardRank.KING),
 ]
 
-DECKS_IN_SHOE: int = 6
-FORCE_RESHUFFLE: int = (52 * DECKS_IN_SHOE) * 3 / 4
-
-import pdb
-
 
 def shuffle_shoe(shoe: list[Card]) -> None:
     """Shuffle shoe in place."""
@@ -139,7 +135,7 @@ def shuffle_shoe(shoe: list[Card]) -> None:
 def create_shoe() -> list[Card]:
     "Create a shoe at the start of the match.  Returned shoe is shuffled."
     shoe: list[Card]
-    shoe = [card for i in range(DECKS_IN_SHOE) for card in UNSHUFFLED_DECK]
+    shoe = [card for i in range(HouseRules.DECKS_IN_SHOE) for card in UNSHUFFLED_DECK]
     shuffle_shoe(shoe)
     return shoe
 
@@ -147,6 +143,43 @@ def create_shoe() -> list[Card]:
 def display_shoe(shoe: list[Card]) -> None:
     for card in shoe:
         print(f"""{card.rank.value}{card.suite.value}""")
+
+
+#
+# House Rules
+#
+
+
+class HouseRules:
+    DECKS_IN_SHOE: int = 6
+    FORCE_RESHUFFLE: int = ((52 * DECKS_IN_SHOE) * 3) / 4
+
+    # True => Must stand after Ace split -- stand on the Ace plus the one card dealt after split
+    NO_MORE_CARDS_AFTER_SPLITTING_ACES: bool = True
+
+    # [9, 10, 11] aka range(9, 12) => "Reno Rules"
+    DOUBLE_DOWN_ON_TOTAL: list[int] = [i for i in range(1, 22)]
+
+    DOUBLE_DOWN_AFTER_SPLIT: bool = True
+
+    # 3 => turn one hand into no more than 4 hands
+    SPLITS_PER_HAND: int = 3
+
+    # rank match like K-K always can split, values match allows K-10 split
+    SPLIT_ON_VALUE_MATCH: bool = True
+
+    DEALER_HITS_HARD_ON: bool = 16  # or less
+    DEALER_HITS_SOFT_ON: bool = 17  # or less
+
+    # 1.5 => 3 to 2 payout, 1.2 => 6 to 5 payout
+    NATURAL_BLACKJACK_PAYOUT: float = 1.5
+
+    SURRENDER_ALLOWED: bool = True
+
+
+#
+# Players and Dealer
+#
 
 
 class PlayerHand:
@@ -222,8 +255,12 @@ class PlayerHand:
             card1 = self.cards[0]
             card2 = self.cards[1]
             # check card value or rank, depending on hiuse rules
-            if card1.rank == card2.rank:
-                return True
+            if HouseRules.SPLIT_ON_VALUE_MATCH:
+                if CARD_VALUE(card1.rank) == CARD_VALUE(card2.rank):
+                    return True
+            else:
+                if card1.rank == card2.rank:
+                    return True
         return False
 
     def add_card(self, card: Card):
@@ -237,7 +274,7 @@ class DealerHand(PlayerHand):
 
 class PlayerHands:
     hands: list[PlayerHand]
-    HAND_LIMIT: int = 4
+    HAND_LIMIT: int = HouseRules.SPLITS_PER_HAND
 
     def add_hand(self, bet: int):
         self.hands.append(PlayerHand(bet=bet))
@@ -303,6 +340,283 @@ class Dealer:
         return self.hand.cards[1]
 
 
+#
+# Basic Strategy:
+#     https://en.wikipedia.org/wiki/Blackjack#Basic_strategy
+#
+
+
+class PlayerDecision(StrEnum):
+    STAND = "stand"
+    HIT = "hit"
+    DOUBLE = "double-down"
+    SPLIT = "split"
+    SURRENDER = "surrender"
+
+
+# to make decision tables readable:
+S = "stand"
+H = "hit"
+Dh = "double-down-if-allowed-or-hit"
+Ds = "double-down-if-allowed-or-stand"
+SP = "split"
+# U => Surrender, in a world of too many Ss
+Uh = "surrender-if-allowed-or-hit"
+Us = "surrender-if-allowed-or-stand"
+Usp = "surrender-if-allowed-or-split"
+NO = "no-decision"
+
+
+def convert_to_player_decision(decision: str) -> PlayerDecision:
+    if decision == S:
+        return PlayerDecision.STAND
+    elif decision == H:
+        return PlayerDecision.HIT
+    elif decision == Dh:
+        return PlayerDecision.DOUBLE
+    elif decision == Ds:
+        return PlayerDecision.HIT
+    elif decision == SP:
+        return PlayerDecision.HIT
+    elif decision == Uh:
+        return PlayerDecision.HIT
+    elif decision == Us:
+        return PlayerDecision.HIT
+    elif decision == Usp:
+        return PlayerDecision.HIT
+    else:
+        return PlayerDecision.STAND
+
+
+RANK_TO_INDEX = {
+    CardRank.ACE: 1,
+    CardRank.TWO: 2,
+    CardRank.THREE: 3,
+    CardRank.FOUR: 4,
+    CardRank.FIVE: 5,
+    CardRank.SIX: 6,
+    CardRank.SEVEN: 7,
+    CardRank.EIGHT: 8,
+    CardRank.NINE: 9,
+    CardRank.TEN: 10,
+    CardRank.JACK: 11,
+    CardRank.QUEEN: 12,
+    CardRank.KING: 13,
+}
+
+# Use "fmt: skip" to force black to leave these table alone.
+# Use NO since we live in a zero index world.
+_PAIRS_DECISION = [
+    # 0   A   2   3   4   5   6   7   8   9  10   J   Q   K
+    [NO, NO, NO, NO, NO, NO, NO, NO, NO, NO, NO, NO, NO, NO],  # fmt: skip
+    # player pair card: Ace  x  dealer top card
+    [NO, SP, SP, SP, SP, SP, SP, SP, SP, SP, SP, SP, SP, SP],  # fmt: skip
+    # player pair card: 2  x  dealer top card
+    [NO,  H, SP, SP, SP, SP, SP, SP,  H,  H,  H,  H,  H,  H],  # fmt: skip
+    # player pair card: 3  x  dealer top card
+    [NO,  H, SP, SP, SP, SP, SP, SP,  H,  H,  H,  H,  H,  H],  # fmt: skip
+    # player pair card: 4  x  dealer top card
+    [NO,  H,  H,  H, H, SP, SP,  H,  H,  H,  H,   H,  H,  H],  # fmt: skip
+    # player pair card: 5  x  dealer top card
+    [NO,  H, Dh, Dh, Dh, Dh, Dh, Dh, Dh, Dh,  H,  H,  H,  H],  # fmt: skip
+    # player pair card: 6 x  dealer top card
+    [NO,  H, SP, SP, SP, SP, SP,  H,  H,  H,  H,  H,  H,  H],  # fmt: skip
+    # player pair card: 7  x  dealer top card
+    [NO,  H, SP, SP, SP, SP, SP, SP,  H,  H,  H,  H,  H,  H],  # fmt: skip
+    # player pair card: 8  x  dealer top card
+    [NO, Usp, SP, SP, SP, SP, SP, SP, SP, SP, SP, SP, SP, SP],  # fmt: skip
+    # player pair card: 9  x  dealer top card
+    [NO,  S, SP, SP, SP, SP, SP,  S, SP, SP,  S,  S,  S,  S],  # fmt: skip
+    # player pair card: 10  x  dealer top card
+    [NO,  S,  S,  S,  S,  S,  S,  S,  S,  S,  S,  S,  S,  S],  # fmt: skip
+    # player pair card: J  x  dealer top card
+    [NO,  S,  S,  S,  S,  S,  S,  S,  S,  S,  S,  S,  S,  S],  # fmt: skip
+    # player pair card: Q  x  dealer top card
+    [NO,  S,  S,  S,  S,  S,  S,  S,  S,  S,  S,  S,  S,  S],  # fmt: skip
+    # player pair card: K  x  dealer top card
+    [NO,  S,  S,  S,  S,  S,  S,  S,  S,  S,  S,  S,  S,  S],  # fmt: skip
+]
+
+
+def create_pairs_decision() -> dict[CardRank, str]:
+    """
+    Turn the _PAIRS_DECISION table into a rank based dictiopnary:
+        PAIRS_DECISION: dict[CardRank, str] = {
+            CardRank.ACE: {
+                CardRank.ACE: SP, CardRank.TWO: SP, ..., CardRank.KING: SP,
+            }, ...
+        }
+    """
+    decisions: dict[CardRank, str] = {}
+
+    for player_pair_card_rank in CardRank:
+        decisions[player_pair_card_rank] = {}
+
+    for player_pair_card_rank in CardRank:
+        player_index: int = RANK_TO_INDEX[player_pair_card_rank]
+        for dealer_top_card_rank in CardRank:
+            dealer_index: int = RANK_TO_INDEX[dealer_top_card_rank]
+            decision: str = _PAIRS_DECISION[player_index][dealer_index]
+            decisions[player_pair_card_rank][dealer_top_card_rank] = decision
+
+    return decisions
+
+
+PAIRS_DECISION: dict[CardRank, str] = create_pairs_decision()
+
+_HARD_TOTAL_DECISION = [
+    # 0   A   2   3   4   5   6   7   8   9  10   J   Q   K
+    [NO, NO, NO, NO, NO, NO, NO, NO, NO, NO, NO, NO, NO, NO],  # fmt: skip
+    # hard total: 1  x  dealer top card
+    [NO, NO, NO, NO, NO, NO, NO, NO, NO, NO, NO, NO, NO, NO],  # fmt: skip
+    # hard total: 2  x  dealer top card
+    [NO, NO, NO, NO, NO, NO, NO, NO, NO, NO, NO, NO, NO, NO],  # fmt: skip
+    # hard total: 3  x  dealer top card
+    [NO, NO, NO, NO, NO, NO, NO, NO, NO, NO, NO, NO, NO, NO],  # fmt: skip
+    # hard total: 4  x  dealer top card
+    [NO, NO, NO, NO, NO, NO, NO, NO, NO, NO, NO, NO, NO, NO],  # fmt: skip
+    # hard total: 5  x  dealer top card
+    [NO,  H,  H,  H,  H,  H,  H,  H,  H,  H,  H,  H,  H,  H],  # fmt: skip
+    # hard total: 6  x  dealer top card
+    [NO,  H,  H,  H,  H,  H,  H,  H,  H,  H,  H,  H,  H,  H],  # fmt: skip
+    # hard total: 7  x  dealer top card
+    [NO,  H,  H,  H,  H,  H,  H,  H,  H,  H,  H,  H,  H,  H],  # fmt: skip
+    # hard total: 8  x  dealer top card
+    [NO,  H,  H,  H,  H,  H,  H,  H,  H,  H,  H,  H,  H,  H],  # fmt: skip
+    # hard total: 9  x  dealer top card
+    [NO,  H,  H,  H,  H,  H,  H,  H,  H,  H,  H,  H,  H,  H],  # fmt: skip
+    # hard total: 10  x  dealer top card
+    [NO,  H,  H, Dh, Dh, Dh, Dh,  H,  H,  H,  H,  H,  H,  H],  # fmt: skip
+    # hard total: 10  x  dealer top card
+    [NO,  H,  H, Dh, Dh, Dh, Dh, Dh, Dh, Dh,  H,  H,  H,  H],  # fmt: skip
+    # hard total: 11  x  dealer top card
+    [NO, Dh, Dh, Dh, Dh, Dh, Dh, Dh, Dh, Dh, Dh, Dh, Dh, Dh],  # fmt: skip
+    # hard total: 12  x  dealer top card
+    [NO,  H,  H,  H,  S,  S,  S,  H,  H,  H,  H,  H,  H,  H],  # fmt: skip
+    # hard total: 13  x  dealer top card
+    [NO,  H,  S,  S,  S,  S,  S,  H,  H,  H,  H,  H,  H,  H],  # fmt: skip
+    # hard total: 14  x  dealer top card
+    [NO,  H,  S,  S,  S,  S,  S,  H,  H,  H,  H,  H,  H,  H],  # fmt: skip
+    # hard total: 15  x  dealer top card
+    [NO, Uh,  S,  S,  S,  S,  S,  H,  H,  H, Uh, Uh, Uh, Uh],  # fmt: skip
+    # hard total: 16  x  dealer top card
+    [NO, Uh,  S,  S,  S,  S,  S,  H,  H, Uh, Uh, Uh, Uh, Uh],  # fmt: skip
+    # hard total: 17  x  dealer top card
+    [NO, Us,  S,  S,  S,  S,  S,  S,  S,  S,  S,  S,  S,  S],  # fmt: skip
+    # hard total: 18  x  dealer top card
+    [NO,  S,  S,  S,  S,  S,  S,  S,  S,  S,  S,  S,  S,  S],  # fmt: skip
+    # hard total: 19  x  dealer top card
+    [NO,  S,  S,  S,  S,  S,  S,  S,  S,  S,  S,  S,  S,  S],  # fmt: skip
+    # hard total: 20  x  dealer top card
+    [NO,  S,  S,  S,  S,  S,  S,  S,  S,  S,  S,  S,  S,  S],  # fmt: skip
+    # hard total: 21  x  dealer top card
+    [NO,  S,  S,  S,  S,  S,  S,  S,  S,  S,  S,  S,  S,  S],  # fmt: skip
+]
+
+
+def create_hard_total_decision() -> list[dict[CardRank, str]]:
+    """
+    Turn _HARD_TOTAL_DECISION table into
+        HARD_TOTAL_DECISION = [
+            ...
+            {CardRank.ACE: H, CardRank.TWO: H, ..., CardRank.KING: H}, # [5]
+            ...
+        ]
+    """
+    decisions = [{} for i in range(22)]
+    for hard_total in range(22):
+        for rank in CardRank:
+            decision: str = _HARD_TOTAL_DECISION[hard_total][CARD_VALUE[rank]]
+            decisions[hard_total][rank] = decision
+    return decisions
+
+
+HARD_TOTAL_DECISION: list[dict[CardRank, str]] = create_hard_total_decision()
+
+
+_SOFT_TOTAL_DECISION = [
+    # 0   A   2   3   4   5   6   7   8   9  10   J   Q   K
+    [NO, NO, NO, NO, NO, NO, NO, NO, NO, NO, NO, NO, NO, NO],  # fmt: skip
+    # soft total: 1  x  dealer top card
+    [NO, NO, NO, NO, NO, NO, NO, NO, NO, NO, NO, NO, NO, NO],  # fmt: skip
+    # soft total: 2  x  dealer top card
+    [NO, NO, NO, NO, NO, NO, NO, NO, NO, NO, NO, NO, NO, NO],  # fmt: skip
+    # soft total: 3  x  dealer top card
+    [NO, NO, NO, NO, NO, NO, NO, NO, NO, NO, NO, NO, NO, NO],  # fmt: skip
+    # soft total: 4  x  dealer top card
+    [NO, NO, NO, NO, NO, NO, NO, NO, NO, NO, NO, NO, NO, NO],  # fmt: skip
+    # soft total: 5  x  dealer top card
+    [NO, NO, NO, NO, NO, NO, NO, NO, NO, NO, NO, NO, NO, NO],  # fmt: skip
+    # soft total: 6  x  dealer top card
+    [NO, NO, NO, NO, NO, NO, NO, NO, NO, NO, NO, NO, NO, NO],  # fmt: skip
+    # soft total: 7  x  dealer top card
+    [NO, NO, NO, NO, NO, NO, NO, NO, NO, NO, NO, NO, NO, NO],  # fmt: skip
+    # soft total: 8  x  dealer top card
+    [NO, NO, NO, NO, NO, NO, NO, NO, NO, NO, NO, NO, NO, NO],  # fmt: skip
+    # soft total: 9  x  dealer top card
+    [NO, NO, NO, NO, NO, NO, NO, NO, NO, NO, NO, NO, NO, NO],  # fmt: skip
+    # soft total: 10  x  dealer top card
+    [NO, NO, NO, NO, NO, NO, NO, NO, NO, NO, NO, NO, NO, NO],  # fmt: skip
+    # soft total: 11  x  dealer top card
+    [NO, NO, NO, NO, NO, NO, NO, NO, NO, NO, NO, NO, NO, NO],  # fmt: skip
+    # soft total: 12 (A, A)  x  dealer top card
+    [NO,  H,  H,  H,  H,  H, Dh,  H,  H,  H,  H,  H,  H,  H],  # fmt: skip
+    # soft total: 13 (A, 2)  x  dealer top card
+    [NO,  H,  H,  H,  H, Dh, Dh,  H,  H,  H,  H,  H,  H,  H],  # fmt: skip
+    # soft total: 14 (A, 3)  x  dealer top card
+    [NO,  H,  H,  H,  H, Dh, Dh,  H,  H,  H,  H,  H,  H,  H],  # fmt: skip
+    # soft total: 15 (A, 4)  x  dealer top card
+    [NO,  H,  H,  H, Dh, Dh, Dh,  H,  H,  H,  H,  H,  H,  H],  # fmt: skip
+    # soft total: 16 (A, 5)  x  dealer top card
+    [NO,  H,  H,  H, Dh, Dh, Dh,  H,  H,  H,  H,  H,  H,  H],  # fmt: skip
+    # soft total: 17 (A, 6)  x  dealer top card
+    [NO,  H,  H, Dh, Dh, Dh, Dh,  H,  H,  H,  H,  H,  H,  H],  # fmt: skip
+    # soft total: 18 (A, 7)  x  dealer top card
+    [NO,  H, Ds, Ds, Ds, Ds, Ds,  S,  S,  H,  H,  H,  H,  H],  # fmt: skip
+    # soft total: 19 (A, 8)  x  dealer top card
+    [NO,  S,  S,  S,  S,  S, Ds,  S,  S,  S,  S,  S,  S,  S],  # fmt: skip
+    # soft total: 20 (A, 9)  x  dealer top card
+    [NO,  S,  S,  S,  S,  S,  S,  S,  S,  S,  S,  S,  S,  S],  # fmt: skip
+    # soft total: 20 (A, 9)  x  dealer top card
+    [NO,  S,  S,  S,  S,  S,  S,  S,  S,  S,  S,  S,  S,  S],  # fmt: skip
+]
+
+
+def create_soft_total_decision() -> list[dict[CardRank, str]]:
+    """
+    Turn _SOFT_TOTAL_DECISION table into
+        SOFT_TOTAL_DECISION = [
+            ...
+            {CardRank.ACE: H, CardRank.TWO: H, ..., CardRank.KING: H}, # [5]
+            ...
+        ]
+    """
+    decisions = [{} for i in range(22)]
+    for soft_total in range(22):
+        for rank in CardRank:
+            decision: str = _SOFT_TOTAL_DECISION[soft_total][CARD_VALUE[rank]]
+            decisions[soft_total][rank] = decision
+    return decisions
+
+
+SOFT_TOTAL_DECISION: list[dict[CardRank, str]] = create_soft_total_decision()
+
+
+class BasicStrategy:
+    @staticmethod
+    def determine_play(
+        dealer_top_card: Card, player_hand: PlayerHand
+    ) -> PlayerDecision:
+        is_first_decision: bool = len(player_hand) == 2 and not player_hand.from_split
+        if is_first_decision:
+            if HouseRules.SURRENDER_ALLOWED:
+                pass
+
+
+#
+# Game
+#
+
 player1: Player = Player()
 player2: Player = Player()
 
@@ -329,7 +643,7 @@ class BlackJack:
         self.players = players
 
     def play_game(self):
-        if self.shoe_top > FORCE_RESHUFFLE:
+        if self.shoe_top > HouseRules.FORCE_RESHUFFLE:
             self.reshuffle_shoe()
 
         # who is playing?
@@ -395,7 +709,7 @@ class BlackJack:
         )
 
 
-# pdb.set_trace()
+pdb.set_trace()
 
 bj = BlackJack()
 bj.play_game()
