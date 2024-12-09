0c37e705e6070705c69c91b96c57a3f897bcb17f
Brian West <bwest@xometry.com>
2024-12-09 18:38:47 -0500

    doc

--- go/blackjack/main.go
+++ go/blackjack/main.go
@@ -5,7 +5,7 @@
 // 3. Stuct field names that start with a lower case letter => private
 // 4. Unit test files must end in "_test.go".  Test functions therein
 // must have the prefix "Test"
-// 5. The ":=" declaration + assignment operator does not take a type, FTW.
+// 5. The ":=" is the declaration + assignment operator does not take a type, FTW.
 // 6. ":=" and "=" are maddenly different
 // 6.1 ":=" only works in a function context
 // 6.2 ":=" has no explicit type => avoid for aggregate types and references thereto
@@ -13,16 +13,17 @@
 // 8. Go does not support constant arrays, maps or slices.
 // 9. Methods are added outside the struct ... aka a receiver function ...
 // "func (self *<struct>) AddCard()" is a receiver function.
-// 9.2 "func (self <struct>) AddCard()" is EVIL, the value the struct instance
+// 9.1 "func (self <struct>) AddCard()" is EVIL, the value the struct instance
 // is copied and perhaps modified by call which is NOT what is desired EVER
-// the idiom should always be used; "func (self *<struct>) AddCard()"
+// the idiom should always be used; "func (self *<struct>) AddCard()" ie
+// always define receiver functions with a reference tot he struct
 // 10. Tuple type has no intrinsic support ... workaround is to define then use struct
 // 11. gofmt is not configurable, no way to disable formatting for a block of code
 // 12. python cares about newline and indents; go cares about newlines
 // 13. WRT <struct instance?>.<member>, the "." notation is the same
 // for both instance and instance reference
 // 13.1 Avoid ":=" since type is implicit => do not know if an instance
-// or  instance reference is being created
+// or instance reference is being created
 
 package main
 
